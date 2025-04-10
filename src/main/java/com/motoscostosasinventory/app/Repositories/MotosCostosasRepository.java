baf20421b36d75deb84fd0ca8d5ab6a8d147f2e2
AlejitoRR <earoncanciocolrei@gmail.com>
2025-04-10 08:14:36 -0500

    Agregué pruebas y modifiqué AppApplication

--- src/main/java/com/motoscostosasinventory/app/Repositories/MotosCostosasRepository.java
+++ src/main/java/com/motoscostosasinventory/app/Repositories/MotosCostosasRepository.java
@@ -1,6 +1,5 @@
 package com.motoscostosasinventory.app.Repositories;
 
-
 import com.motoscostosasinventory.app.Entities.MotosCostosasEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,10 +9,8 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface MotosCostosasRepository extends JpaRepository<MotosCostosasEntity, UUID>{
+public interface MotosCostosasRepository extends JpaRepository<MotosCostosasEntity, UUID> {
 
     Page<MotosCostosasEntity> findAllByMarcaContaining(String marca, Pageable pageable);
 
-    @Override
-    Page<MotosCostosasEntity> findAll(Pageable pageable);
 }
